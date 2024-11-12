ee9536bb2be8af6db32d81ca58394bea7034f788
Lim-seok <lsg15951@gmail.com>
2024-11-12 15:51:34 +0900

    feat: Add PlayerController.cs
    플레이어 컨트롤러 추가.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Move")]
+    public float moveSpeed;
+    private Vector2 curMovementInput;
+
+    private Rigidbody rigidbody;
+
+    private void Awake()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            curMovementInput = Vector2.zero;
+        }
+    }
+    private void Move()
+    {
+        Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+        dir *= moveSpeed;
+        dir.y = rigidbody.velocity.y;
+
+        rigidbody.velocity = dir;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
